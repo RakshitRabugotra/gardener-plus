f7fd61f390603099ae7bf5128045a97571068e19
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-03-13 14:34:59 +0530

    fix: resolve linking to asset

--- src/constants/Images.ts
+++ src/constants/Images.ts
@@ -1,4 +1,4 @@
 export default {
-  splashLogo: require('../assets/images/splash-logo.png'),
+  splashLogo: require('../assets/images/splash-icon.png'),
   addIcon: require('../assets/res/add-50.png'),
 }
